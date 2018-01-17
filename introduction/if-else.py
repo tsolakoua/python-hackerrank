8ab78e622f54c055ad00fc22511a69d8ba93a24b
Anna Tsolakou <tsolakou.a@gmail.com>
2018-01-17 16:14:09 +0100

    Add files via upload

---
+++ introduction/if-else.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(raw_input())
+    if (n % 2 == 1) or (n % 2 == 0 and n >= 6 and n <=20):
+        print "Weird"
+    elif (n % 2 == 0 and n >= 2 and n <=5) or (n % 2 == 0 and n >= 20):
+        print "Not Weird"
