b905f8ea32fe7c75e96153158efd4503f32e8325
Anna Tsolakou <tsolakou.a@gmail.com>
2018-02-19 16:56:21 +0100

    Update intro_sets.py

--- sets/intro_sets.py
+++ sets/intro_sets.py
@@ -1,1 +1,10 @@
-
+def average(array):
+    my_set = set(array)
+    leng = len(my_set)
+    return sum(my_set)/leng
+  
+if __name__ == '__main__':
+   n = int(input())
+   arr = list(map(int, input().split()))
+   result = average(arr)
+   print(result)
